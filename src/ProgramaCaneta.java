70aa8f733c68811d895604c68d8f7f22f36ad13c
Clebson Araújo <44328963+Clebson-desenvolvedor@users.noreply.github.com>
2020-03-28 16:54:33 -0300

    Mais interações com o usuário.

--- src/ProgramaCaneta.java
+++ src/ProgramaCaneta.java
@@ -5,6 +5,28 @@ public class ProgramaCaneta {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("\tBem-vindo ao programa Caneta\n\n"
+				+ "\tEntre com as especificações:\n\n");
+		
+		System.out.println("Entre com o modelo:");
+		String modelo = sc.next();
+		
+		System.out.println("Entre com a cor:");
+		String cor = sc.next();
+		
+		System.out.println("Entre com a espessura da ponta:");
+		double ponta = sc.nextDouble();
+		
+		Caneta caneta = new Caneta(modelo, cor, ponta);
+		
+		caneta.statusAtual();
+		
+		int opcao = 0;
+		
+		do {
+			caneta.menu(opcao);
+		} while (opcao != 6);
 	
 		sc.close();
 	}
